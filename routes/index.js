24826529dbf515621ef1c1931649b2d3a2089c5a
lekiagospel@gmail.com <lekiagospel@gmail.com>
2019-04-17 15:32:52 +0100

    basic route setup

--- routes/index.js
+++ routes/index.js
@@ -8,3 +8,42 @@ var Test = require('../controllers/test');
 router.get('/', Test.first);
 
 module.exports = router;
+
+
+// Updated Router here
+/* GET home page. */
+//router.get("/", Home.index);
+router.get("/", function(req, res, next) {
+  JobModel.find(function(err, jobs) {
+    res.render("index", { title: "Remote Job Alert", contents: jobs });
+  });
+});
+
+
+// GET About us page
+router.get("/about", Home.aboutUs);
+
+//Admin Page
+router.get('/admin', Home.admin);
+
+router.post('/admin', function(req, res, next){
+	if (req.body.username && req.body.password) {
+    Admin.authenticate(req.body.username, req.body.password, function (error, admin) {
+      if (error || !admin) {
+        var err = new Error('Wrong username or password.');
+        err.status = 401;
+        return next(err);
+      } else {
+        req.session.adminId = admin._id;
+        return res.redirect('/managefees');
+      }
+    });
+}
+});
+
+
+// GET Contact us page
+router.get("/contact", Home.contactUs);
+
+// GET FAQS us page
+router.get("/faqs", Home.faqs);
