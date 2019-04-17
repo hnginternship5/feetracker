22e49ad194ec3887d089f340b698a363255f4c5c
Abubakar <talk2asquare@gmail.com>
2019-04-17 13:50:37 +0100

    Little correction

--- controllers/class.js
+++ controllers/class.js
@@ -4,7 +4,7 @@ const Classes = {
     async create(req, res, next){
         const queryText = {}
         try {
-            const createdClass = await studentDb.create(queryText);
+            const createdClass = await classDb.create(queryText);
             return res.status(201).json(createdClass);
         } catch(error){
             return res.status(400).send(error);
@@ -13,7 +13,7 @@ const Classes = {
     async get_all_students(req, res, next){
         const queryText = {};
         try {
-            const foundClasses = await studentDb.find(queryText);
+            const foundClasses = await classDb.find(queryText);
             return res.status(200).json(foundClasses);
         } catch(error){
             return res.status(400).send(error);
@@ -22,7 +22,7 @@ const Classes = {
     async get_all_students_json(req, res, next){
         const queryText = {};
         try {
-            const foundclasses = await studentDb.find(queryText);
+            const foundclasses = await classDb.find(queryText);
             return res.status(200).json(foundClasses);
         } catch(error){
             return res.status(400).send(error);
@@ -33,7 +33,7 @@ const Classes = {
            _id: req.params.class_id 
         }
         try {
-            const foundClass = await studentDb.findOne(queryText);
+            const foundClass = await classDb.findOne(queryText);
             return res.status(200).json(foundClass);
         }catch(error){
             return res.status(400).send(error);
@@ -44,7 +44,7 @@ const Classes = {
             _id: req.params.class_id
         }
         try {
-            const editedClass = await studentDb.findOne(queryText);
+            const editedClass = await classDb.findOne(queryText);
             return res.status(200).json(editedClass);
         }catch(error){
             return res.status(400).send(error);
@@ -56,7 +56,7 @@ const Classes = {
         }
         const updateText = {}
         try {
-            const updatedClass = await studentDb.findOneAndUpdate(queryText, updateText);
+            const updatedClass = await classDb.findOneAndUpdate(queryText, updateText);
             return res.status(200).json(updatedClass);
         }catch(error){
             return res.status(400).send(error);
@@ -67,7 +67,7 @@ const Classes = {
             _id: req.params.class_id
         }
         try {
-            const deletedClass = await studentDb.findOneAndUpdate(queryText, updateText);
+            const deletedClass = await classDb.findOneAndUpdate(queryText, updateText);
             return res.status(200).json(deletedClass);
         }catch(error){
             return res.status(400).send(error);
