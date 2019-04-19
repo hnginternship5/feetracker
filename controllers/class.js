94fdc55f79c873f8192c11b3543b22f04134bcf5
hackthievist <sobogunifeoluwa@gmail.com>
2019-04-19 01:42:00 +0100

    Refactored models, controllers and added new models/controllers for term and account, added eslint to project to help with linting

--- controllers/class.js
+++ controllers/class.js
@@ -1,79 +1,81 @@
-const classDb = require("./classPromise");
+const classDb = require('./promise').ClassDb;
 
-const Classes = {
-    async create(req, res, next){
-        const queryText = {}
-        try {
-            const createdClass = await classDb.create(queryText);
-            return res.status(201).json(createdClass);
-        } catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async get_all_classes(req, res, next){
-        const queryText = {};
-        try {
-            const foundClasses = await classDb.find(queryText);
-            return res.status(200).json(foundClasses);
-        } catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async get_all_parent_classes(req, res, next){
-        const queryText = {
-            parent_id: null
-        };
-        try {
-            const foundClasses = await classDb.find(queryText);
-            return res.status(200).json(foundClasses);
-        } catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async get_one_class(req, res, next){
-        const queryText = {
-           _id: req.params.class_id 
-        }
-        try {
-            const foundClass = await classDb.findOne(queryText);
-            return res.status(200).json(foundClass);
-        }catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async get_class_arms(req, res, next) {
-        const queryText = {
-            isArm: true,
-            parent_id: req.params.class_id
-        };
-        try {
-            const foundArms = await classDb.findOne(queryText);
-            return res.status(200).json(foundArms);
-        }catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async update_class(req, res, next){
-        const queryText = {
-            _id: req.params.class_id
-        }
-        const updateText = {}
-        try {
-            const updatedClass = await classDb.findOneAndUpdate(queryText, updateText);
-            return res.status(200).json(updatedClass);
-        }catch(error){
-            return res.status(400).send(error);
-        }
-    },
-    async delete_class(req, res, next){
-        const queryText = {
-            _id: req.params.class_id
-        }
-        try {
-            const deletedClass = await classDb.findOneAndDelete(queryText);
-            return res.status(200).json(deletedClass);
-        }catch(error){
-            return res.status(400).send(error);
-        }
+const Class = {
+  async create(req, res) {
+    const queryText = req.body;
+    try {
+      const createdClass = await classDb.create(queryText);
+      return res.status(201).json(createdClass);
+    } catch (error) {
+      return res.status(400).send(error);
     }
-}
+  },
+  async get_all_classes(req, res) {
+    const queryText = {};
+    try {
+      const foundClasses = await classDb.find(queryText);
+      return res.status(200).json(foundClasses);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async get_all_parent_classes(req, res) {
+    const queryText = {
+      parent_id: null,
+    };
+    try {
+      const foundClasses = await classDb.find(queryText);
+      return res.status(200).json(foundClasses);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async get_one_class(req, res) {
+    const queryText = {
+      id: req.params.class_id,
+    };
+    try {
+      const foundClass = await classDb.findOne(queryText);
+      return res.status(200).json(foundClass);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async get_class_arms(req, res) {
+    const queryText = {
+      isArm: true,
+      parent_id: req.params.class_id,
+    };
+    try {
+      const foundArms = await classDb.findOne(queryText);
+      return res.status(200).json(foundArms);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async update_class(req, res) {
+    const queryText = {
+      id: req.params.class_id,
+    };
+    const updateData = req.body;
+    try {
+      const updatedClass = await classDb.findOneAndUpdate(queryText, updateData);
+      return res.status(200).json(updatedClass);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+  async delete_class(req, res) {
+    const queryText = {
+      id: req.params.class_id,
+    };
+    try {
+      const deletedClass = await classDb.findOneAndDelete(queryText);
+      return res.status(200).json(deletedClass);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+  },
+};
+
+module.exports = Class;
